95b6c655f9c16c5566be12bce6f9c2fc26a9d00f
Jinstronda <joaopanizzutti@gmail.com>
2024-09-22 18:59:37 +0100

    Integrated Z-SCORE normalization and changed the larning rate. Now it has a 0.1% error and i was able to lower the tries from 100k to less than 3k, still can optimize more

--- Multiple Linear Regression.py
+++ Multiple Linear Regression.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from mpl_toolkits.mplot3d import Axes3D
 def calcular_hypothesis(x_train,w_vector,b):
-    return np.dot(x_train/10,w_vector/10) + b
+    return np.dot(x_train,w_vector) + b
 def calcular_custo_e_vetorgradiente(x_test,y_test,w_vector,b): # Calcula o custo do modelo
     m = len(y_test)
     n = np.shape(x_test)[1]
@@ -28,23 +28,34 @@ def logfunctioncost(iterations,cost):
     plt.plot(iterations,cost)
     plt.show()
 
+def normalization(x_train): # Vai NORMALIZAR a X function e retornar os valores de deviação + o x normalizado
+    mean = np.mean(x_train,axis = 0)  # Calcula a média
+    std = np.std(x_train,axis= 0) # Calcula a standard deviation
+    x_norm = (x_train - mean) / std
+    return mean,std,x_norm
 
-x_train = np.array([
+
+x_train0 = np.array([ # X antes da normalização
     [2104, 5, 1],   # Exemplo 1: Tamanho da casa, Número de quartos, Número de banheiros
     [1416, 3, 2],   # Exemplo 2
     [852, 2, 1],    # Exemplo 3
     [1236, 4, 2]    # Exemplo 4
 ])
+
 # Valores reais correspondentes (preços ou qualquer variável alvo)
 y_train = np.array([460, 232, 178, 240])
 m = len(y_train) # Numero de Exemplos
+mean, std, x_train = normalization(x_train0)
+print(x_train)
 n = np.shape(x_train)[1] # Numero de Features
+
+
 w_vector = np.zeros(n)
 b = 0
-l = 0.00005 # Learning Rate
+l = 0.001 # Learning Rate
 cost = [] # Cria uma lista para o custo
 iterations = [] # Cria uma lista para iterations
-for times in range(1000000):
+for times in range(5000):
     derivative_w, derivative_b = calcular_custo_e_vetorgradiente(x_train,y_train,w_vector,b)
     w_vector = w_vector - (l*derivative_w)
     b = b - (l*derivative_b)
@@ -54,18 +65,16 @@ cost = np.array(cost)
 iterations = np.array(iterations)
 costandfunction = np.column_stack((cost, iterations))
 
-while False: # Mostra a linha prevista comparada aos pontos target
-    plt.plot(calcular_hypothesis(x_train,w_vector,b), label= "Previsão")
-    plt.plot(y_train, label= "Previsão")
-    plt.scatter(range(len(y_train)), y_train, label= "Valores De Treino")
-    plt.show()
 
+plt.plot(calcular_hypothesis(x_train,w_vector,b), label= "Previsão")
+plt.plot(y_train, label= "Previsão")
+plt.scatter(range(len(y_train)), y_train, label= "Valores De Treino")
+plt.show()
 
 
 # Plotando apenas as primeiras 10.000 iterações
 start = 0
-end = 100000
-plt.yscale('log')
+end = 5000
 plt.xlabel("Iterações")
 plt.ylabel("Custo (Escala Log)")
 plt.plot(iterations[start:end], cost[start:end])  # Seleciona as primeiras 10.000 iterações
