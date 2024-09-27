7dd97c84cc957f52f73d54ba55b37d8ba39b6fef
Jinstronda <joaopanizzutti@gmail.com>
2024-09-27 22:55:56 +0100

    Integrated Lambda, Better Polynomial for the terms

--- Multiple Linear Regression.py
+++ Multiple Linear Regression.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 from mpl_toolkits.mplot3d import Axes3D
+from sklearn.preprocessing import PolynomialFeatures
 def calcular_hypothesis(x_train,w_vector,b):
     return np.dot(x_train,w_vector) + b
 def calcular_custo_e_vetorgradiente(x_test,y_test,w_vector,b): # Calcula o custo do modelo
@@ -38,11 +39,10 @@ def normalization(x_train): # Vai NORMALIZAR a X function e retornar os valores 
 def new_values_normalization(mean,std, house): # Vai pegar um valor de uma casa e normalizar ele para poder ser usado com nosso data set
         house_norm = (house - mean) / std
         return house_norm
-def polynomial(x_train,n):  # Vai transformar as features em POLINOMIOS para ter uma relação não linear
-    for i in range(n):
-        x_train[:,i] = x_train[:,i] ** (i + 1)
-    print(x_train)
-    return x_train
+def polynomial(x_train):  # Vai transformar as features em POLINOMIOS para ter uma relação não linear
+    poly = PolynomialFeatures(degree=4, include_bias=False)
+    poly_train = poly.fit_transform(x_train)
+    return poly_train
 def plot_model(x_train,w_vector,y_train,b): # Função para fazer o grafico de forma melhor
     xaxis = np.arange(len(y_train))
     plt.scatter(xaxis, y_train)
@@ -54,28 +54,89 @@ def plot_cost_function(start,end,iterations,cost):
     plt.plot(iterations[start:end], cost[start:end])  # Seleciona as primeiras 10.000 iterações
     plt.show()
     # Compara o custo pelas iterações
-x_train0 = np.array([ # X antes da normalização
-    [2500, 4, 2, 10],  # Example 1
-    [1500, 3, 2, 20],  # Example 2
-    [1800, 3, 1, 15],  # Example 3
-    [1200, 2, 1, 30]  # Example 4
+
+
+x_train0 = np.array([
+    [1500, 3, 10, 5],
+    [1800, 4, 15, 8],
+    [1200, 2, 20, 3],
+    [2000, 4, 5, 10],
+    [1600, 3, 8, 4],
+    [1700, 3, 12, 6],
+    [1900, 4, 7, 9],
+    [1100, 2, 25, 2],
+    [1300, 2, 22, 4],
+    [1400, 3, 18, 5],
+    [2100, 5, 9, 11],
+    [1000, 2, 30, 1],
+    [2200, 5, 6, 12],
+    [1250, 2, 24, 3],
+    [1750, 4, 13, 7],
+    [1650, 3, 10, 4],
+    [1850, 4, 6, 8],
+    [1350, 3, 17, 4],
+    [1450, 3, 19, 5],
+    [1550, 3, 11, 6],
+    [2300, 5, 4, 12],
+    [1950, 4, 5, 10],
+    [1700, 3, 8, 5],
+    [1050, 2, 28, 2],
+    [1400, 3, 15, 6],
+    [2000, 4, 6, 11],
+    [1800, 4, 9, 9],
+    [1500, 3, 12, 6],
+    [1200, 2, 20, 3],
+    [2100, 5, 7, 12]
+])
+y_train = np.array([
+    400,
+    500,
+    300,
+    600,
+    450,
+    470,
+    550,
+    280,
+    330,
+    380,
+    610,
+    270,
+    620,
+    310,
+    480,
+    460,
+    540,
+    340,
+    390,
+    420,
+    630,
+    560,
+    470,
+    290,
+    380,
+    600,
+    520,
+    410,
+    320,
+    610
 ])
 
-# Valores reais correspondentes (preços ou qualquer variável alvo)
-y_train = np.array([460, 232, 178, 240])
-m = len(y_train) # Numero de Exemplos
-mean, std, x_train = normalization(x_train0)
-n = np.shape(x_train)[1] # Numero de Features
-print(f"{x_train} BEFORE polynomial")
-x_train = polynomial(x_train,n)
 
-w_vector = np.zeros(n)
+x_train = polynomial(x_train0)
+mean, std, x_train = normalization(x_train)
+
+m = len(y_train) # Numero de Exemplos
+n = np.shape(x_train)[1] # Numero de Features
 b = 0
-l = 0.01 # Learning Rate
+l = 0.001 # Learning Rate
+w_vector = np.zeros(n)
+lambdafeature = 0.01 # Regularization rate para NÃO TER OVERFITTING
 cost = [] # Cria uma lista para o custo
 iterations = [] # Cria uma lista para iterations
-for times in range(1000):
+for times in range(10000):
     derivative_w, derivative_b = calcular_custo_e_vetorgradiente(x_train,y_train,w_vector,b)
+    lambdacalc = (lambdafeature / m) * w_vector
+    derivative_w += lambdacalc  # Aplica a Regularização
     w_vector = w_vector - (l*derivative_w)
     b = b - (l*derivative_b)
     cost.append(calcular_custo(x_train,y_train,w_vector,b))
@@ -86,55 +147,8 @@ costandfunction = np.column_stack((cost, iterations))
 
 
 
-plot_model(x_train, w_vector, y_train, b)
+plot_model(x_train,w_vector,y_train,b)
 
 
 # Plotando apenas as primeiras 10.000 iterações
 
-if False:
-    # Tentando Prever valores de casas
-    house1 = [2000, 4, 2]
-    house2 = [4000,9,5]
-    house3 = [5000,10,5]
-    # Calcula a previsão para cada casa
-    house1 = new_values_normalization(mean,std,house1)
-    house2 = new_values_normalization(mean,std,house2)
-    house3 = new_values_normalization(mean,std,house3)
-    house1prediction = (calcular_hypothesis(house1,w_vector, b))
-    house2prediction = (calcular_hypothesis(house2,w_vector, b))
-    house3prediction = (calcular_hypothesis(house3,w_vector, b))
-    # Tentando Prever valores de casas
-    house1 = [2000, 4, 2]
-    house2 = [4000,9,5]
-    house3 = [5000,10,5]
-    house1 = new_values_normalization(mean,std,house1)
-    house2 = new_values_normalization(mean,std,house2)
-    house3 = new_values_normalization(mean,std,house3)
-
-    # Calcula a previsão para cada casa
-    house1prediction = calcular_hypothesis(house1,w_vector, b)
-    house2prediction = calcular_hypothesis(house2,w_vector, b)
-    house3prediction = calcular_hypothesis(house3,w_vector, b)
-
-    # Plotando as previsões e os valores reais
-    plt.plot(calcular_hypothesis(x_train,w_vector,b), label= "Previsão de Treino")
-    plt.scatter(range(len(y_train)), y_train, color='blue', label="Valores Reais de Treino", zorder=5)
-
-    # Adicionando as previsões das novas casas
-    plt.scatter([len(y_train)], house1prediction, color='red', label="Previsão Casa 1", zorder=6)
-    plt.scatter([len(y_train)+1], house2prediction, color='green', label="Previsão Casa 2", zorder=6)
-    plt.scatter([len(y_train)+2], house3prediction, color='purple', label="Previsão Casa 3", zorder=6)
-    # Criando um novo vetor de previsões, incluindo as novas casas
-    extended_predictions = list(calcular_hypothesis(x_train, w_vector, b)) + [house1prediction, house2prediction, house3prediction]
-
-    # Criando um novo vetor de índices para acomodar as novas casas
-    extended_indices = list(range(len(y_train))) + [len(y_train), len(y_train)+1, len(y_train)+2]
-
-    # Plotando as previsões e os valores reais
-    plt.plot(extended_indices, extended_predictions, label="Previsão de Treino + Casas Novas")
-    plt.scatter(range(len(y_train)), y_train, color='blue', label="Valores Reais de Treino", zorder=5)
-    # Adiciona legendas e rótulos
-    plt.xlabel("Exemplos")
-    plt.ylabel("Preços Previstos / Valores Reais")
-    plt.legend()
-    plt.show()
